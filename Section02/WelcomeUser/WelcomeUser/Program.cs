8ed438e6d4de707467e2bf4f37477d8cfa192cf3
Ehab Zaky <ehabzaky@gmail.com>
2019-08-30 19:13:13 +0300

    updates

---
+++ Section02/WelcomeUser/WelcomeUser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeUser
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("What is your name? ");
+      string userName = Console.ReadLine();
+      Console.Write("Welcome, ");
+      Console.WriteLine(userName);
+    }
+  }
+}
